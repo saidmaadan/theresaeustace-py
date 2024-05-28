8ac197a85159f84ac22e6e35fc7027b17630343e
Said Maadan <saidmaadan@gmail.com>
2024-05-28 11:00:25 -0500

    Modified settings.py

--- theresaeustace/settings.py
+++ theresaeustace/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 # DEBUG = os.getenv('DEBUG', default=False, cast=bool),
 ALLOWED_HOSTS = ["www.theresaeustace.com",
-                 "http://www.theresaeustace.com", "https://www.theresaeustace.com", "theresaeustace.com", "https://theresaeustace.up.railway.app", "theresaeustace.up.railway.app", "*"]
+                 "http://www.theresaeustace.com", "https://www.theresaeustace.com", "theresaeustace.com", "https://theresaeustace.up.railway.app", "theresaeustace.up.railway.app", "0.0.0.0", "localhost", "*"]
 
 CSRF_TRUSTED_ORIGINS = ['https://theresaeustace.up.railway.app',
                         'https://theresaeustace.com', 'https://www.theresaeustace.com']
